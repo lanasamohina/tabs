a6554182c700c559f292003bd4a6fa44134096a9
lanasamohina <lana.samohina@gmail.com>
2018-03-16 23:47:28 +0200

    added slider

--- main.js
+++ main.js
@@ -2,49 +2,74 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init () {
     const tabsBtn = document.querySelector('.js-tabs');
-    tabs(tabsBtn);
+    tabs(tabsBtn, 'fade');
 
-    ///const tabsBtn2 = document.querySelector('.tabs-2 .js-tabs');
-    //tabs(tabsBtn2);
+    const tabsBtn2 = document.querySelector('.js-tabs-slider');
+    tabs(tabsBtn2,'slide');
 }
 
-function tabs (tabsBtn) {
+function tabs (tabsBtn, effect = 'fade' ) {
+
+    let effectList = ['fade', 'slide'];
+    if (effectList.indexOf(effect) === -1){
+        effect = 'fade';
+    }
+
     const activeTab = 'tabs__content-item--active';
     const activeBtn = 'tabs__item--active';
     let bar = document.createElement('div');
     bar.classList.add('tabs_bar');
     tabsBtn.appendChild(bar);
     let startTab = tabsBtn.querySelector(`.${activeBtn}`);
-
     //moving bar to the initial position
     moveBar(bar,startTab);
 
+    if (effect == 'slide') {
+        calcRowWidth();
+    }
+
     tabsBtn.addEventListener('click', ev => {
         if (ev.target.tagName === 'A') {
-        const anchor = ev.target.getAttribute('href');
+            const anchor = ev.target.getAttribute('href');
+            //moving bar
+            moveBar(bar,ev.target);
+            let tabconteiner = ev.target.closest('.tabs');
+            switchTab(anchor, effect, tabconteiner);
+            switchActiveBtn(ev.target);
+            ev.preventDefault();
+        }
+    });
 
-        //moving bar
-        moveBar(bar,ev.target);
-        switchTab(anchor);
-        switchActiveBtn(ev.target);
-    }
-});
-
-    function switchTab (selector) {
-        let tab = document.querySelector(selector);
+    function switchTab (selector, effect, tabconteiner) {
+        let tab = tabconteiner.querySelector(selector);
         let parent = tab.closest('.tabs__content');
+        switch(effect) {
+            case 'fade':
+                fadeFunction(parent, tab);
+                break;
+            case 'slide':
+                slideFunction(parent, tab);
+                break;
+        }
+    }
 
+    function fadeFunction(parent, tab){
         //fade effect
         parent.querySelector(`.${activeTab}`).style.opacity = 0;
         tab.style.opacity = 0;
-         setTimeout(function(){
-             parent.querySelector(`.${activeTab}`)
-                 .classList.remove(activeTab);
-                 tab.classList.add(activeTab);
-                 setTimeout(function () {
-                     tab.style.opacity = 1;
-                 },100);
-         },400);
+        setTimeout(function(){
+            parent.querySelector(`.${activeTab}`)
+                .classList.remove(activeTab);
+            tab.classList.add(activeTab);
+            setTimeout(function () {
+                tab.style.opacity = 1;
+            },100);
+        },400);
+    }
+
+    function slideFunction(parent, tab) {
+        parent.style.transform ='translateX(-'+ tab.offsetLeft+'px)';
+
     }
 
     function switchActiveBtn (selector) {
@@ -64,5 +89,11 @@ function tabs (tabsBtn) {
         bar.style.width = width+'px';
         let barheigth = bar.offsetHeight;
         bar.style.top = (height-barheigth)+'px';
+    }
+
+    function calcRowWidth() {
+        let n = document.querySelectorAll('.tabs-slider .tabs__item').length;
+        let itemWidth = document.querySelector('.tabs-slider .tabs__content-item').offsetWidth;
+        document.querySelector('.tabs-slider .tabs__content').style.width = itemWidth*n +'px';
     }
 }
